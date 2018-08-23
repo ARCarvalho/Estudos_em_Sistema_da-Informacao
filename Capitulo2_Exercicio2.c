4d7af84e3a2c6fc94155160332e9526f2431f666
ARCarvalho <42628335+ARCarvalho@users.noreply.github.com>
2018-08-23 00:41:48 -0300

    Add files via upload

---
+++ Capitulo2_Exercicio2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Escreva um programa que leia um numero inteiro e depois o imprima. */
+
+int main () {
+    int x;
+
+    scanf("%d", &x);
+    printf("%d", x);
+    return 0;
+}
