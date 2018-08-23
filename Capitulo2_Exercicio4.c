4039fabdcf9df0bd3f69a5425659744facdf3b8c
ARCarvalho <42628335+ARCarvalho@users.noreply.github.com>
2018-08-23 00:55:22 -0300

    Add files via upload

---
+++ Capitulo2_Exercicio4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Faca um programa que leia um numero inteiro e depois o imprima usando o operador "%f". Veja o que acontece. */
+
+int main() {
+    int x;
+
+    scanf("%d", &x);
+    printf("%f\n", x);
+    return 0;
+}
---
+++ Capitulo2_Exercicio5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Faca um programa que leia um numero do tipo float e depois o imprima usando o operador "%d". Veja o que aconteceu. */
+
+int main() {
+    float x;
+
+    scanf("%f", &x);
+    printf("%d\n", x);
+    return 0;
+}
