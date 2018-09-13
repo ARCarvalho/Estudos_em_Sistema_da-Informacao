2ebbe4c617938952e1375f9d90c56be4dc8b9742
Marcos F. Caetano <mfcaetano@unb.br>
2018-09-13 14:39:06 -0300

    -apenas reformatando o codigo.

--- cap4/Ex07.c
+++ cap4/Ex07.c
@@ -16,24 +16,21 @@ int main() {
     if((estado1 == 'M') && (estado2 == 'G')){
         valor_com_imposto = valor_produto * 1.07;
         printf("\nO valor do produto com imposto para o estado %c%c eh R$%0.2lf", estado1, estado2, valor_com_imposto);
-    } else{
-        if((estado1 == 'S') && (estado2 == 'P')) {
-            valor_com_imposto = valor_produto * 1.12;
-            printf("\nO valor do produto com imposto para o estado %c%c eh R$%0.2lf", estado1, estado2, valor_com_imposto);
-        } else{
-            if((estado1 == 'R') && (estado2 == 'J')) {
-                valor_com_imposto = valor_produto * 1.15;
-                printf("\nO valor do produto com imposto para o estado %c%c eh R$%0.2lf", estado1, estado2,valor_com_imposto);
-            } else{
-                if((estado1 == 'M') && (estado2 == 'S')) {
-                    valor_com_imposto = valor_produto * 1.08;
-                    printf("\nO valor do produto com imposto para o estado %c%c eh R$%0.2lf", estado1, estado2, valor_com_imposto);
-                } else {
-                    printf("\nVoce digitou a sigla do estado errado.");
-                }
 
-            }
-        }
+    } else if((estado1 == 'S') && (estado2 == 'P')) {
+        valor_com_imposto = valor_produto * 1.12;
+        printf("\nO valor do produto com imposto para o estado %c%c eh R$%0.2lf", estado1, estado2, valor_com_imposto);
+
+    } else if((estado1 == 'R') && (estado2 == 'J')) {
+        valor_com_imposto = valor_produto * 1.15;
+        printf("\nO valor do produto com imposto para o estado %c%c eh R$%0.2lf", estado1, estado2,valor_com_imposto);
+
+    } else if((estado1 == 'M') && (estado2 == 'S')) {
+        valor_com_imposto = valor_produto * 1.08;
+        printf("\nO valor do produto com imposto para o estado %c%c eh R$%0.2lf", estado1, estado2, valor_com_imposto);
+
+    } else {
+        printf("\nVoce digitou a sigla do estado errado.");
     }
 
     return 0;
