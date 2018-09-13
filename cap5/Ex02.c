d6093455440a89efe37c038342a889472415aad7
Marcos F. Caetano <mfcaetano@unb.br>
2018-09-13 14:52:43 -0300

    -decrementando o valor de i.

--- cap5/Ex02.c
+++ cap5/Ex02.c
@@ -4,15 +4,14 @@
 /* Faca um programa que leia um numero inteiro positivo N e imprima todos os numeros naturais de 0 ate N em ordem decrescente. */
 
 int main() {
-    int numero, aux1, aux2;
+    int numero, i;
 
     printf("Digite um numero inteiro positivo: ");
     scanf("%d", &numero);
 
-    aux2 = numero;
-    for (aux1 = 0; aux1 <= numero; aux1++) {
 
-        printf("%d\n", aux2--);
+    for (i = numero; i >= 0; i--) {
+        printf("%d\n", i);
     }
 
     return 0;
