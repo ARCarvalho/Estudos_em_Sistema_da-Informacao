af20a0c5a7f9a7dae4012afbace94a889a4dcdea
Marcos F. Caetano <mfcaetano@gmail.com>
2019-03-28 14:23:57 -0300

    -generalizando implementaçãde vetor e ponteiro.

--- Cap10/Ex08.c
+++ Cap10/Ex08.c
@@ -3,7 +3,7 @@
 
 /* Crie uma funcao que receba dois parametros: um vetor e um valor do mesmo tipo do vetor. A funcao deverah preencher os elementos de vetor com esse valor. Nao utilize indices para percorrer o vetor, apenas aritmetica de ponteiros. */
 
-int Preencher_vetor(int vetor[5], int valor){
+/*int Preencher_vetor(int vetor[5], int valor){
     int *ponteiro1 = &vetor[0];
     int *ponteiro2 = &vetor[4];
     int *ponteiro3;
@@ -17,7 +17,16 @@ int Preencher_vetor(int vetor[5], int valor){
     }
 
     return 0;
+}*/
+
+void Preencher_vetor(int *vetor, int tamanho, int valor){
+    int* aux = vetor;
+    
+    for(int i = 0; i < tamanho; i++, aux++){
+        *aux = valor;
+    }
 }
+
 
 int main() {
     int vetor[5];
@@ -26,6 +35,11 @@ int main() {
     printf("Digite um valor inteiro para preencher um vetor de 5 posicoes: ");
     scanf("%d", &valor);
 
-    Preencher_vetor(vetor, valor);
+    Preencher_vetor(vetor, 5, valor);
+    
+    
+    for(int i = 0; i < 5; i++)
+        printf("%d %d\n", i, vetor[i]);
+    
     return 0;
 }
