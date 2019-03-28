d097d8f240ef62391b84497eead4698f238b00c6
Marcos F. Caetano <mfcaetano@gmail.com>
2019-03-28 14:16:05 -0300

    -resolvendo problema Ex10.

--- Cap10/Ex10.c
+++ Cap10/Ex10.c
@@ -8,15 +8,21 @@ int main() {
 
     printf("Digite um numero inteiro: ");
     scanf("%d", &a);
+    
+    b = &a;
 
-    *b = 2 * a;
-    printf("\nO dobro de A eh %d.", *b);
+    //*b = 2 * a;
+    printf("\nO dobro de A eh %d.", (2 * (*b)) );
 
-    **c = 3 * a;
-    printf("\nO triplo de A eh %d.", **c);
+    c = &b;
+    
+    //**c = 3 * a;
+    printf("\nO triplo de A eh %d.", (3 * (**c)));
 
-    ***d = 4 * a;
-    printf("\nO quadruplo de A eh %d.", ***d);
+    d = &c;
+    
+    //***d = 4 * a;
+    printf("\nO quadruplo de A eh %d.", (4 * (***d)));
 
 
     return 0;
